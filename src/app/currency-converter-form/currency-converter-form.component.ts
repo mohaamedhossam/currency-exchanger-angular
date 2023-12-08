fa8965e88e980049bba0c91997c5c857fce258c6
mohaamedhossam <mohamed.7oss98@gmail.com>
2023-12-08 23:10:30 +0200

    fourth commit

--- src/app/currency-converter-form/currency-converter-form.component.ts
+++ src/app/currency-converter-form/currency-converter-form.component.ts
@@ -1,35 +1,60 @@
+import { Component } from '@angular/core';
+import axios from 'axios';
+import { CurrencyRates } from '../Models/Result';
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
 @Component({
   selector: 'app-currency-converter-form',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './currency-converter-form.component.html',
-  styleUrls: ['./currency-converter-form.component.css'],
+  styleUrl: './currency-converter-form.component.css',
 })
 export class CurrencyConverterFormComponent {
-  @Input() initialValue: number = 1;
-  @Input() fromCurrency: string = 'USD';
-  @Input() toCurrency: string = 'EUR';
-  @Input() currencyOptions: string[] = ['EUR', 'JPY', 'CYN'];
-  @Output() convertCurrency = new EventEmitter<{
-    value: string;
-    from: string;
-    to: string;
-  }>();
-
-  onChange(event: any): void {
-    this.convertCurrency.emit({
-      value: event.target.value,
-      from: this.fromCurrency,
-      to: this.toCurrency,
+  ngOnInit(): void {
+    this.toCurrency = 'EUR';
+    this.fromCurrency = 'USD';
+    axios(
+      'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oLf23dmp8n5z4DGEl4D6GYIsRyjJzO95KdwK4Dc9'
+    ).then((response) => {
+      this.currencyData = response.data.data;
+      this.currencies = Object.keys(this.currencyData).map(
+        (currency) => currency
+      );
+      //console.log(this.currencies[8]);
+      //console.log(currencyData['AUD']);
+      //console.log(response.data.data);
+      // this.Results = response.data.data;
+      // this.currencies = Object.keys(this.Results).map((currency) => currency);
+      // console.log(Object.values(this.Results).map((currency) => currency));
+      console.log(this.toCurrency);
+      console.log(this.fromCurrency);
+      this.Result = this.Amount * this.currencyData[this.toCurrency];
+      // console.log(response.data.data);
     });
-    // console.log(this.value, this.fromCurrency);
+  }
+  public currencyData: { [currency: string]: number } = {};
+  public currencies: string[] = [];
+  toCurrency: string = '';
+  fromCurrency: string = '';
+  Amount: number = 1;
+  Result: number = 1;
+  toSelected(value: string): void {
+    this.toCurrency = value;
+    console.log(this.toCurrency);
+  }
+  fromSelected(value: string): void {
+    this.fromCurrency = value;
+    console.log(this.fromCurrency);
   }
 
-  handleConvert(): void {
-    // Implement your conversion logic here
-    // Update the converted value and emit the output
+  input3(value: any) {
+    this.Amount = value;
+    console.log(this.Amount);
   }
+  handleConvert() {
+    this.Result = this.Amount * this.currencyData[this.toCurrency];
+    console.log(this.Result);
+  }
+  // public Results: CurrencyRates[] = [];
+  // public currencies: String[] = [];
 }
